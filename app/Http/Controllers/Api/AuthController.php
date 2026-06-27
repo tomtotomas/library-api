eec063ef1d4f18257f5bf5d1f0c1e52ecb6979f0
tomas <tomas.a.gomez.zb@gmail.com>
2026-06-27 20:49:11 -0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,17 +10,6 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    /*public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        if (auth()->attempt($credentials)) {
-            $token = auth()->user()->createToken('API Token')->plainTextToken;
-            return response()->json(['token' => $token]);
-        }
-
-        return response()->json(['error' => 'Unauthorized'], 401);
-    }*/
 
     public function login(Request $request)
     {
