553199c4f73e51f0c1513413a483718cc7167f0c
tomas <tomas.a.gomez.zb@gmail.com>
2026-06-27 21:06:13 -0300

    index fixed

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [App\Http\Controllers\Api\UserController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
-Route::get('/users', [App\Http\Controllers\Api\UserController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     
     Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
     Route::get('/me', [App\Http\Controllers\Api\AuthController::class, 'me']);
-    //Route::get('/users', [App\Http\Controllers\Api\UserController::class, 'index'])->middleware('check.role:admin');
+    Route::get('/users', [App\Http\Controllers\Api\UserController::class, 'index'])->middleware('role:admin');
 
 });
